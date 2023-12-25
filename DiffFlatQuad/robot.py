5a5ed9d2f976077a0b0372321c420b9707525595
Rooholla-Khorrambakht <learn.kho@gmail.com>
2023-12-24 19:40:19 -0500

    simulator and robot model added.

---
+++ DiffFlatQuad/robot.py
@@ -0,0 +1,108 @@
+import pygame
+import numpy as np
+from sympy import symbols, Matrix, sin, cos
+
+class PlanerQuadrotor():
+    def __init__(self, dt=0.01, rendering = True):
+        self.x = np.array([0.0, 0.])  # Start at the center of the screen
+        self.v = np.array([0., 0.])  # Start at the center of the screen
+        self.omega = 0.
+        self.theta = 0.0
+        self.F = np.array([0., 0.])
+        self.mass = 1.0
+        self.J = 0.1
+        self.L = 0.2
+        self.gravity = np.array([0, 9.8])
+        self.rendering = rendering
+        self.dt = dt 
+        self.symbolic_state = Matrix(symbols('x_1,x_2,x_3,x_4,x_5,x_6'))
+        self.symbolic_input = Matrix(symbols('u_1,u_2'))
+        if rendering:
+            pygame.init()
+            self.size = (1024, 768)
+            self.screen = pygame.display.set_mode(self.size)
+            pygame.display.set_caption('Quadrotor Simulator')
+            self.clock = pygame.time.Clock()
+            self.screen.fill((255, 255, 255))  # Clear screen
+            self.color = (0, 0, 0)  # Red
+            self.meter_to_pix = 100
+            self.robot_img = pygame.image.load('DiffFlatQuad/quad.png')
+            self.original_image = self.robot_img  # Store the original image to avoid degradation over time
+            self.rect = self.robot_img.get_rect(center=self.x+np.array(self.size)//2)
+
+    def reset(self):
+        self.x = np.array([0., 0.])  # Start at the center of the screen
+        self.v = np.array([0., 0.])  # Start at the center of the screen
+        self.omega = 0.
+        self.theta = 0.
+        self.F = np.array([0., 0.])
+        self.mass = 1.0
+        self.gravity = np.array([0, 9.8])
+
+    def step(self, T, F):
+        self.x += self.v * self.dt
+        self.v += self.dt*((F*np.array([np.sin(self.theta), np.cos(self.theta)]))/self.mass-self.gravity)
+        self.theta += self.omega*self.dt
+        self.omega += T/self.J
+        if self.rendering:
+            self.render()
+
+    def render(self):
+        self.screen.fill((255, 255, 255))  # Clear screen
+        angle_degrees = np.degrees(self.theta) 
+        self.robot_img = pygame.transform.rotate(self.original_image, -angle_degrees)
+        self.rect = self.robot_img.get_rect(center=np.array(self.size)/2 + np.diag([1,-1])@self.x*self.meter_to_pix)
+        self.screen.blit(self.robot_img, self.rect)
+        pygame.display.flip()
+
+    def running(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return False
+        return True
+
+    def getSymbolicF(self):
+        """
+        return the system dynamics f(x)
+        x1 = x
+        x2 = v_x
+        x3 = y
+        x4 = v_y
+        x5 = theta
+        x6 = omega
+        """
+        x = symbols('x_1,x_2,x_3,x_4,x_5,x_6')
+        m, J, g = symbols('m,J, g')
+        f = Matrix([[
+            x[1],
+            0,
+            x[3],
+            -m*g,
+            x[4],
+            0
+        ]])
+        return f.T
+    
+    def getSymbolicG(self):
+        """
+        return g(x). Not that u1 is thrust (F) and u2 is torque (T)
+        """
+        x = symbols('x_1,x_2,x_3,x_4,x_5,x_6')
+        m, J, g = symbols('m,J, g')
+        g = Matrix([[
+            0,
+            (1/m)*sin(x[4]),
+            0,
+            (1/m)*cos(x[4]),
+            0,
+            0
+        ],
+        [
+            0,
+            0,
+            0,
+            0,
+            0,
+            1/J
+        ]])
+        return g.T
---
+++ simulator_test.py
@@ -0,0 +1,15 @@
+import time
+import pygame
+
+from DiffFlatQuad.robot import PlanerQuadrotor
+
+def main():
+    quadrotor = PlanerQuadrotor()
+    while quadrotor.running():
+        quadrotor.step(T=0.0001, F=9.8)
+        time.sleep(0.01)
+
+    pygame.quit()
+
+if __name__ == '__main__':
+    main()
